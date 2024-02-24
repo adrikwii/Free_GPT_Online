fade2fd04295804fec20d1b13bc43a29939f36a8
adrikiwii <44178126+adrikwii@users.noreply.github.com>
2024-02-24 17:00:22 +0100

    Update GPT.py

--- GPT.py
+++ GPT.py
@@ -17,6 +17,7 @@ with tab1 :
         messages = st.container(height=300)
         if prompt := st.chat_input("Say something"):
             messages.chat_message("user").write(prompt)
+            st.toast('En cours de génération ...')
             response = g4f.ChatCompletion.create(
                     model=g4f.models.gpt_4,
                     provider=g4f.Provider.You,
@@ -24,14 +25,15 @@ with tab1 :
                     stream=True,
                     )
             messages.chat_message("assistant").write(response)
+            st.toast('Terminée :smile:')
 with tab2 :
     Image = st.container(height=550)
     if generationPic := st.chat_input("Image"):
         Image.chat_message("user").write(generationPic)
         st.toast('En cours de génération ...')
         response =  "https://image.pollinations.ai/prompt/"+generationPic
-        st.toast('Terminée :smile:')
         Image.chat_message("assistant").image(response,width=400)
+        st.toast('Terminée :smile:')
 col4, col5, col6 = st.columns(3)
 st.divider()
 with col6:
