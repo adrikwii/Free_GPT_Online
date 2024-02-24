02226aaf59c1139ac6043a4b7b5a9c983690f6ee
adrikiwii <44178126+adrikwii@users.noreply.github.com>
2024-02-24 16:37:37 +0100

    Update GPT.py

--- GPT.py
+++ GPT.py
@@ -4,8 +4,7 @@ import g4f
 
 st.set_page_config(page_title="Free_GPT",page_icon=":robot_face:",layout="wide")
 col1, col2, col3 = st.columns(3)
-with col2 :
-    col4,col5,col6 = st.columns(3)
+col4,col5,col6 = st.columns(3)
     st.header("""
 ╭━━━┳━━━┳━━━┳━━━╮╱╭━━━┳━━━┳━━━━╮
 ┃╭━━┫╭━╮┃╭━━┫╭━━╯╱┃╭━╮┃╭━╮┃╭╮╭╮┃
