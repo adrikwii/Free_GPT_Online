18eb86910266f14d0699f6130cd2cb8a212ca0cf
adrikiwii <44178126+adrikwii@users.noreply.github.com>
2024-02-26 12:03:29 +0100

    Update GPT.py

--- GPT.py
+++ GPT.py
@@ -28,7 +28,7 @@ with tab1 :
                         i += 1
             messages.chat_message("user",avatar="Icon/utilisateur.png").write(prompt)
             st.toast('En cours de génération ...')
-            with messages.chat_message("assistant",avatar="Icon/robot.png").spinner("..."):
+            with messages.spinner("..."):
                 response = g4f.ChatCompletion.create(
                     model=g4f.models.gpt_4,
                     provider=g4f.Provider.You,
