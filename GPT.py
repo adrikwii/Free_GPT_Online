4dddaf6b64643d0840dc279a857adc69127147ce
adrikiwii <44178126+adrikwii@users.noreply.github.com>
2024-02-26 14:04:26 +0100

    Update GPT.py

--- GPT.py
+++ GPT.py
@@ -43,13 +43,13 @@ with tab1 :
             messages.chat_message("user",avatar="Icon/utilisateur.png").write(prompt)
             st.toast('En cours de génération ...')
             with messages.chat_message("assistant",avatar="Icon/robot.png"):
-		    with st.spinner(""):
-			    response = g4f.ChatCompletion.create(
-				    model=g4f.models.gpt_4,
-				    provider=g4f.Provider.You,
-				    messages=[{"role": "user", "content": prompt}],
-			    )
-		    st.write(response)
+		with st.spinner(""):
+			response = g4f.ChatCompletion.create(
+				model=g4f.models.gpt_4,
+				provider=g4f.Provider.You,
+				messages=[{"role": "user", "content": prompt}],
+			)
+		st.write(response)
             st.session_state.historique.append([prompt,response])
             st.toast('Terminé :smile:')
 with tab2 :
