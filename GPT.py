c3db771738936300810387fcc5761b34dbf1e6ca
adrikiwii <44178126+adrikwii@users.noreply.github.com>
2024-02-26 11:02:32 +0100

    Update GPT.py

--- GPT.py
+++ GPT.py
@@ -1,8 +1,7 @@
 import streamlit as st
 import g4f
-
-st.set_page_config(page_title="Free_GPT",page_icon=":robot_face:",layout="wide")
 historique = []
+st.set_page_config(page_title="Free_GPT",page_icon=":robot_face:",layout="wide")
 col1, col2, col3 = st.columns(3)
 with col2:
     st.text("""
